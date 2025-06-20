6da9562f8f51cfcb43fd2f98266f0ad79bf588bd
gauthammariyappan <60974224+gauthammariyappan@users.noreply.github.com>
2025-06-20 18:06:34 +0530

    Update random.js

--- random.js
+++ random.js
@@ -30,17 +30,24 @@ function getRandomName() {
 
 function getAppKey(){
   let appKey = sessionStorage.getItem('appKey');
+	let DC = 'ca'	
   if (!appKey) {
     if (Math.random() < 0.5) {
       appKey = '4b3cd1b44d6f9e7351ff7f5db4eeaa88';//Ravi anna account
+	DC = 'com'
     } else {
       appKey = '126132d2a20325e5ef7e500a2cfa0442';//mywebsite
     }
     sessionStorage.setItem('appKey', appKey);
   }
-  return appKey;
+  return {
+	  appKey: appKey,
+	  dc: DC
+  }
 
 }
+
+const configData = getAppKey();
 
 (function(w,d,s,r,k,h,m){
 	if(w.performance && w.performance.timing && w.performance.navigation) {
@@ -50,7 +57,7 @@ function getAppKey(){
 		(m = window.onerror),(window.onerror = function (b, c, d, f, g) {
 		m && m(b, c, d, f, g),g || (g = new Error(b)),(w[r].q = w[r].q || []).push(["captureException",g]);})
 	}
-})(window,document,'//static.site24x7rum.ca/beacon/site24x7rum-min.js?appKey=','s247r',getAppKey());
+})(window,document,'//static.site24x7rum.'+configData.dc+'/beacon/site24x7rum-min.js?appKey=','s247r',configData.appKey);
 s247r('setWaterfallsSamplingRate',1);
 s247r('userId',getRandomName());
 s247r('recordSession',{
